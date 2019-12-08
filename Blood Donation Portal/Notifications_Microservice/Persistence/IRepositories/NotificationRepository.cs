c843aced68ae4622d571a1db628c69c13c0487b2
Mobius98 <cosmin.sescu@gmail.com>
2019-12-08 17:43:10 +0200

    added notifyUserWhenBloodIsUsed functionality

---
+++ Blood Donation Portal/Notifications_Microservice/Persistence/IRepositories/NotificationRepository.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Notifications_Microservice.Models;
+using Notifications_Microservice.Persistence.Contexts;
+using Notifications_Microservice.Persistence.Repositories;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Notifications_Microservice.Persistence.IRepositories
+{
+    public class NotificationRepository : BaseRepository, INotificationRepository
+    {
+        public NotificationRepository(NotificationsDbContext context) : base(context) { }
+
+        public async Task<IEnumerable<PrivateNotification>> GetNotifications(string email)
+        {
+            var userNotifications =  await _context.PrivateNotifications.Where(n => n.DonorEmail.ToLower().Equals(email.ToLower())).ToListAsync();
+            return userNotifications;
+        }
+
+        public async Task<PrivateNotification> NotifyDonorWhenBloodIsUsed(string email)
+        {
+            PrivateNotification notification = new PrivateNotification
+            {
+                DonorEmail = email
+            };
+            await _context.PrivateNotifications.AddAsync(notification);
+            await _context.SaveChangesAsync();
+            return notification;
+        }
+    }
+}
