a7aed2bef28dbe0e8fd1ba19167576d9b7f1b1f4
BeatriceSt <beatrice.sticea@gmail.com>
2019-12-16 16:16:20 +0200

    Font-end

---
+++ Front-End/blood-donation-portal-app/src/app/login/login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {RegisterComponent} from "../register/register.component";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  constructor() {
+  }
+}
